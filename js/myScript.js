2df2121cc25aef23ff88f122d4ee0878864d4939
Chokolove <carlosll_24@hotmail.com>
2018-05-20 02:21:05 -0500

    change to arrow function

--- js/myScript.js
+++ js/myScript.js
@@ -2,18 +2,14 @@ $('document').ready(function(){
   $('#btn').click(function(){
     var li = $('<li></li>')
     var text = $('#textbox').val()
+    var span = $('<span></span>').text(text)
+    span.click(() => span.toggleClass('checked'))
     var btn = $('<button>X</button>')
     btn.click(() => li.remove())
-    li.append(text)
+    li.append(span)
     li.append(btn)
     $('#list').append(li)
     $('#textbox').val('')
     $('#textbox').focus()
-  });
-
-  $('ul').click(function(ev) {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
-    };
   });
 });
