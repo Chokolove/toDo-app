f609e83e6b7a50fe2e24527a432c6bb51e859825
Chokolove <carlosll_24@hotmail.com>
2018-05-20 01:21:35 -0500

    jquery 2

--- js/myScript.js
+++ js/myScript.js
@@ -1,16 +1,17 @@
-$("document").ready(function(){
-  $("#btn").click(function(){
-    var btn = "<button>X</button>";
-    var li = "<li>"+$("#textbox").val()+"<button class='delete'>X</button></li>"
-    $("#list").append(li);
-    $(".delete").click(function(){
-      this.parentElement.remove();
-    })
-    $("#textbox").val("");
-    $("#textbox").focus();
+$('document').ready(function(){
+  $('#btn').click(function(){
+    var li = $('<li></li>')
+    var text = $('#textbox').val()
+    var btn = $('<button>X</button>')
+    btn.click(() => li.remove())
+    li.append(text)
+    li.append(btn)
+    $('#list').append(li)
+    $('#textbox').val('')
+    $('#textbox').focus()
   });
 
-  $("ul").click(function(ev) {
+  $('ul').click(function(ev) {
     if (ev.target.tagName === 'LI') {
       ev.target.classList.toggle('checked');
     };
