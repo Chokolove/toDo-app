eac77b90a365ed2e50608c821680b53391a36832
Chokolove <carlosll_24@hotmail.com>
2018-05-21 00:43:39 -0500

    Counter add

--- js/myScript.js
+++ js/myScript.js
@@ -3,13 +3,25 @@ $('document').ready(function(){
     var li = $('<li></li>')
     var text = $('#textbox').val()
     var span = $('<span></span>').text(text)
-    span.click(() => span.toggleClass('checked'))
+    span.click(() =>{
+      span.toggleClass('checked')
+      counter()
+    })
     var btn = $('<button>X</button>')
-    btn.click(() => li.remove())
+    btn.click(() =>{
+      li.remove()
+      counter()
+    })
     li.append(span)
     li.append(btn)
     $('#list').append(li)
+    var total = $('li').length
     $('#textbox').val('')
     $('#textbox').focus()
-  });
+  })
+  function counter(){
+    $('#total').text($('li').length)
+    $('#pending').text($('li').length-$('.checked').length)
+    $('#done').text($('.checked').length)
+  }
 });
